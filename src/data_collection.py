d041d53ff3bc4632b5357fdd2abc4e25481cd1ab
giosue <giosue@castellano.de>
2024-04-19 16:59:53 +0200

    Some basic functionality, added data directory

--- src/data_collection.py
+++ src/data_collection.py
@@ -1,5 +1,19 @@
 from datasets import load_dataset
+import pandas as pd
+from utils import save_to_csv, get_first_prompt
 
 hh_dataset = load_dataset("Anthropic/hh-rlhf")
 
 # should process hh-rlhf dataset and create a final csv with the dataset, and save it into \data
+
+# Transform dataset to pandas DataFrame
+hh_train = pd.DataFrame(hh_dataset["train"]) # two columns: "chosen" and "rejected"
+
+# Simple first implementation of process_data that just gets the first human prompt. Note that doing this will result in "chosen" and "rejected" columns
+# containing exactly the same data (the first human prompt)
+def process_data(df: pd.DataFrame):
+    df = get_first_prompt(df, "Assistant")
+    return df
+
+hh_train_processed = process_data(hh_train)
+save_to_csv(hh_train_processed, "hh_train_processsed")
---
+++ src/utils.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+def save_to_csv(df: pd.DataFrame, file_name: str, file_path: str = "../data"):
+    """
+    Save DataFrame to CSV file.
+    """
+    df.to_csv(f"{file_path}/{file_name}.csv", index=False)
+
+def get_first_prompt(df: pd.DataFrame, keyword: str = "Assistant"):
+    """
+    Cut any full prompt up to the first occurence of the keyword for all dataframe columns.
+    Example:  
+    \n\nHuman: What kind of noises did dinosaurs make?\n\nAssistant: Humans and dinosaurs didn’t live at the same time, so it’s really hard to say.
+    -> \n\nHuman: What kind of noises did dinosaurs make?\n\n
+    """
+    for column in df.columns:
+        df[column] = df[column].apply(lambda x: x[:x.find(keyword)] if keyword in x else x)
+    return df
