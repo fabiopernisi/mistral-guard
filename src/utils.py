7cedf94b2b20aa9a2e2dc8690fa754e55c032cae
giosue <giosue@castellano.de>
2024-04-25 22:23:07 +0200

    Add tokenizer template

--- src/utils.py
+++ src/utils.py
@@ -26,13 +26,13 @@ def get_batch_completion(model, tokenizer, texts: List[str], batch_size = 4):
     """
     Load corresponding model from Hugging Face and generate completions for given text.
     """
-    generated = list()
-    for i in tqdm(range(0, len(texts), batch_size)):
+    generated = []
+    for i in tqdm(range(0, len(texts), batch_size), desc = "Generating completions"):
         batch = texts[i:i + batch_size]
-        inputs = tokenizer(batch, truncation = True, padding=True, return_tensors="pt").to("cuda")
+        messages = [{"role": "user", "content": text} for text in batch]
+        inputs = tokenizer.apply_chat_template(messages, tokenize=True, return_tensors="pt").to("cuda")
         # TODO: Talk about parameters, i.e. top_k, max_length, etc.
-        outputs = model.generate(**inputs, max_length = 1024)
-        gen = tokenizer.batch_decode(outputs, skip_special_tokens = True)
-        print(gen)
+        outputs = model.generate(**inputs, max_length=1024)
+        gen = tokenizer.batch_decode(outputs, skip_special_tokens=True)
         generated.extend(gen)
     return generated
