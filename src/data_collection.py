cf3dad2f2cf672273188f0fbb6583f55ea374bc4
giosue <giosue@castellano.de>
2024-05-16 18:19:41 +0000

    Final prompt completion data

--- src/data_collection.py
+++ src/data_collection.py
@@ -11,16 +11,16 @@ def process_data(df: pd.DataFrame):
     model = AutoModelForCausalLM.from_pretrained("cognitivecomputations/WizardLM-33B-V1.0-Uncensored", device_map = "auto", load_in_4bit = True, torch_dtype = torch.bfloat16)
     print("Finished loading model.")
     df["model_completion"] = "NaN"
-    step_size = 2
-    for index in tqdm(range(480, len(df), step_size), desc = "Generating completions"):
+    step_size = 4
+    for index in tqdm(range(0, len(df), step_size), desc = "Generating completions"):
         end_index = min(index + step_size, len(df))
         batch_prompts = df["prompt"][index:end_index].tolist()
         batch_completions = get_batch_completion(model, tokenizer, batch_prompts, batch_size = step_size)
         df.loc[index:end_index-1, "model_completion"] = batch_completions
-        save_to_csv(df, "merged_unsafe_prompts_ongoing_contexts")
+        save_to_csv(df, "../data/new_prompts/all_additional_prompts_ongoing")
     return df
 
-merged_unsafe_prompts = pd.read_csv("../data/rerun_unsafe_prompts.csv")
+merged_unsafe_prompts = pd.read_csv("../data/new_prompts/all_additional_prompts")
 merged_unsafe_prompts_processed = process_data(merged_unsafe_prompts)
 dataset_postprocessed = post_process_model_completion(merged_unsafe_prompts_processed, "model_completion")
-save_to_csv(dataset_postprocessed, "rerun_unsafe_prompts_contexts_completion_postprocessed")
+save_to_csv(dataset_postprocessed, "../data/new_prompts/all_additional_prompts_completion")
